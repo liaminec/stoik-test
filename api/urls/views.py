a10553aa94c488e586954f7ebc0969c43bc82ac9
liamine cheridi <liaminec@gmail.com>
2025-09-25 17:45:08 +0200

    [urls] fixed views upon testing + added docstings for swagger

--- api/urls/views.py
+++ api/urls/views.py
@@ -1,10 +1,9 @@
 from contextlib import closing
 
-from flask import current_app, Response, redirect
-from flask_pydantic import validate
+from flask import current_app, Response, redirect, request
 from pydantic import ValidationError
-from sqlalchemy import text, Connection, TextClause
-from sqlalchemy.exc import IntegrityError, NoResultFound
+from sqlalchemy import text, Connection
+from sqlalchemy.exc import IntegrityError, NoResultFound, MultipleResultsFound
 
 from flaskr.db import get_db
 from urls import urls_bp
@@ -12,26 +11,52 @@ from urls.models import UrlCreate, ShortPath
 from urls.utils import short_path_generator
 
 
-def _create(dbconn: Connection, insert_query: TextClause, url: str) -> tuple[bool, str]:
-    short_path = short_path_generator()
-    try:
-        ShortPath(short_path=short_path)
-    except ValidationError as valerr:
-        return False, str(valerr)
-    try:
-        dbconn.execute(insert_query, {"short_path": short_path, "url": url})
-    except IntegrityError as integ_err:
-        return False, str(integ_err)
-    dbconn.commit()
-    return True, short_path
-
-
 @urls_bp.route("/", methods=["POST"])
-@validate()
-def create(body: UrlCreate):
+def create():
+    """
+    Endpoint creating a short path and linking it to a given url.
+    ---
+    parameters:
+        - name: body
+          in: body
+          description: URL which will be shortened
+          schema:
+            type: object
+            properties:
+              url:
+                type: string
+                example: 'https://www.google.com'
+    responses:
+      201:
+        description: The short path was created.
+        schema:
+            type: object
+            properties:
+                short_path:
+                    type: string
+                    example: 'A1b2C3d'
+    """
     logger = current_app.logger
+    body = request.get_json()
+    try:
+        url_create = UrlCreate(**body)
+    except ValidationError as verr:
+        return verr.json(), 400
+    url = url_create.url
 
-    url = body.url
+    def _create(dbconn: Connection) -> tuple[bool, str]:
+        short_path = short_path_generator()
+        try:
+            ShortPath(short_path=short_path)
+        except ValidationError as valerr:
+            return False, valerr.json()
+        try:
+            dbconn.execute(insert_query, {"short_path": short_path, "url": url})
+        except IntegrityError as integ_err:
+            return False, str(integ_err)
+        dbconn.commit()
+        return True, short_path
+
     check_query = text(
         """
         SELECT short_path
@@ -46,21 +71,28 @@ def create(body: UrlCreate):
             existing_result = conn.execute(check_query, {"url": url}).scalar_one()
         except NoResultFound:
             logger.info("No short path exists for the url %s", url)
-    if existing_result:
-        return Response({"short_path": existing_result}, status=200)
-    insert_query = text(
-        """
-        INSERT INTO urls (short_path, url)
-        VALUES (:short_path, :url)
-        """
-    )
+        except MultipleResultsFound as mulres:
+            logger.error(
+                "Multiple valid results were found for %(url)s, reason: %(err)s"
+                % {"url": url, "err": str(mulres)}
+            )
+            return {
+                "message": "Cannot generate a short path for this URL currently"
+            }, 400
+        if existing_result:
+            return {"short_path": existing_result}, 200
+        insert_query = text(
+            """
+            INSERT INTO urls (short_path, url)
+            VALUES (:short_path, :url)
+            """
+        )
 
-    create_success = False
-    output = ""
-    fail_counter = 0
-    with closing(get_db()) as conn:
+        create_success = False
+        output = ""
+        fail_counter = 0
         while not create_success and fail_counter <= 3:
-            create_success, output = _create(conn, insert_query, url)
+            create_success, output = _create(conn)
             logger.warning("Could not generate short URL, cause: %s", output)
             fail_counter += 1
     if not create_success:
@@ -70,11 +102,25 @@ def create(body: UrlCreate):
             status=400,
         )
     logger.info("Short URL for %s successfully generated", url)
-    return Response({"short_path": output}, status=201)
+    return {"short_path": output}, 201
 
 
 @urls_bp.route("/<string:short_path>", methods=["GET"])
 def path(short_path: str):
+    """
+    Endpoint that redirects to a url linked to a given short path.
+    ---
+    parameters:
+        - name: short_path
+          in: path
+          description: The short path that will redirect to its associated url
+          schema:
+            type: string
+            example: 'A1b2C3d'
+    responses:
+      302:
+        description: Redirection to the URL
+    """
     logger = current_app.logger
 
     select_query = text(
@@ -82,29 +128,32 @@ def path(short_path: str):
         SELECT url, clicks
         FROM urls
         WHERE short_path = :short_path
-        AND (NOW()::date - created_at::date) < 90
-        """
-    )
-    try:
-        with closing(get_db()) as conn:
-            url, clicks = conn.execute(
-                select_query, {"short_path": short_path}
-            ).scalar_one()
-    except NoResultFound as nores:
-        logger.warning(
-            "No url could be found for short path %(short_path)s, reason: %(err)s"
-            % {"short_path": short_path, "err": str(nores)}
-        )
-        return {"message": "Not found"}, 404
-    clicks += 1
-    update_query = text(
-        """
-        UPDATE urls
-        SET clicks = :clicks
-        WHERE short_path = :short_path
+          AND (NOW()::date - created_at::date) < 90
         """
     )
     with closing(get_db()) as conn:
+        try:
+            url, clicks = conn.execute(select_query, {"short_path": short_path}).one()
+        except NoResultFound as nores:
+            logger.error(
+                "No url could be found for short path %(short_path)s, reason: %(err)s"
+                % {"short_path": short_path, "err": str(nores)}
+            )
+            return {"message": "Not found"}, 404
+        except MultipleResultsFound as mulres:
+            logger.error(
+                "Multiple valid results were found for %(short_path)s, reason: %(err)s"
+                % {"short_path": short_path, "err": str(mulres)}
+            )
+            return {"message": "There was an issue with the link"}, 400
+        clicks += 1
+        update_query = text(
+            """
+            UPDATE urls
+            SET clicks = :clicks
+            WHERE short_path = :short_path
+            """
+        )
         conn.execute(update_query, {"clicks": clicks, "short_path": short_path})
         conn.commit()
     return redirect(url, code=302)
