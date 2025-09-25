ddbe87331a4447f8197eadb787de9510b8c65dc3
liamine cheridi <liaminec@gmail.com>
2025-09-25 02:49:51 +0200

    [urls] fixed pydantic models validation

--- api/urls/models.py
+++ api/urls/models.py
@@ -1,29 +1,29 @@
 from urllib.parse import urlparse
 
-from pydantic import BaseModel, field_validator
+from pydantic import BaseModel, model_validator
 
 
 class UrlCreate(BaseModel):
     url: str
 
-    @field_validator("url", mode="after")
-    def validate_url(self, value: str) -> str:
-        result = urlparse(value)
+    @model_validator(mode="after")
+    def validate_url(self) -> "UrlCreate":
+        result = urlparse(self.url)
         is_valid = all([result.scheme, result.netloc])
         if not is_valid:
             raise ValueError("The url is not valid")
-        return value
+        return self
 
 
 class ShortPath(BaseModel):
     short_path: str
 
-    @field_validator("short", mode="after")
-    def validate_short(self, value: str) -> str:
-        if not len(value) == 7:
+    @model_validator(mode="after")
+    def validate_short(self) -> "ShortPath":
+        if not len(self.short_path) == 7:
             raise ValueError("The shortened path should be 7 characters")
-        if not value.isalnum():
+        if not self.short_path.isalnum():
             raise ValueError(
                 "The shortened path should contain only alphanumeric characters"
             )
-        return value
+        return self
